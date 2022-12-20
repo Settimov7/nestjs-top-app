b99ca658f2ee81ecffbe49183d1373ec504da4e3
Settimo <prytkov.nikita.set@gmail.com>
2022-12-20 12:23:33 +0400

    add review tests

---
+++ test/review.e2e-spec.ts
@@ -0,0 +1,80 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from "../src/app.module";
+import { CreateReviewDto } from "../src/review/dto/create-review.dto";
+import { Types, disconnect } from "mongoose";
+import { REVIEW_NOT_FOUND_MESSAGE } from "../src/review/review.constants";
+
+const productId = new Types.ObjectId().toHexString();
+
+const TEST_DTO: CreateReviewDto = {
+  name: 'Test',
+  title: 'Test title',
+  description: 'Test description',
+  rating: 5,
+  productId,
+}
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let createdId: string;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/review/create (POST) - success', async () => {
+    return request(app.getHttpServer())
+      .post('/review/create')
+      .send(TEST_DTO)
+      .expect(201)
+      .then(({ body }: request.Response) => {
+        createdId = body._id;
+
+        expect(createdId).toBeDefined();
+      });
+  });
+
+  it('/review/byProduct/:productId (GET) - success',async () => {
+    return request(app.getHttpServer())
+      .get(`/review/byProduct/${productId}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.length).toBe(1);
+      });
+  });
+
+  it('/review/byProduct/:productId (GET) - fail',async () => {
+    return request(app.getHttpServer())
+      .get(`/review/byProduct/${new Types.ObjectId().toHexString()}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.length).toBe(0);
+      });
+  });
+
+  it('/review/:id (DELETE) - success',() => {
+    return request(app.getHttpServer())
+      .delete(`/review/${createdId}`)
+      .expect(200)
+  });
+
+  it('/review/:id (DELETE) - fail',() => {
+    return request(app.getHttpServer())
+      .delete(`/review/${new Types.ObjectId().toHexString()}`)
+      .expect(404, {
+        statusCode: 404,
+        message: REVIEW_NOT_FOUND_MESSAGE,
+      });
+  });
+
+  afterAll(() => {
+    disconnect();
+  })
+});
