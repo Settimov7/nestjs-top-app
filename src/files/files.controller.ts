beeee0dd03ebeffac8db1d2d100c91b61015bd7f
Settimo <prytkov.nikita.set@gmail.com>
2023-01-15 13:48:34 +0400

    add images converting

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from "@nestjs/platform-express";
 import { JwtAuthGuard } from "../auth/guards/jwt.guard";
 import { FileElementResponse } from "./dto/file-element.response";
 import { FilesService } from "./files.service";
+import { MFile } from "./mFile.class";
 
 @Controller('files')
 export class FilesController {
@@ -13,6 +14,14 @@ export class FilesController {
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('files'))
   async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
-    return this.filesService.saveFiles([file]);
+    const saveArray: MFile[] = [new MFile(file)]
+
+    if (file.mimetype.includes('image')) {
+      const webP = await this.filesService.convertToWebP(file.buffer);
+
+      saveArray.push(new MFile({ originalname: `${file.originalname.split('.')[0]}.wepb`, buffer: webP}))
+    }
+
+    return this.filesService.saveFiles(saveArray);
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { format } from 'date-fns';
 import { path } from "app-root-path";
+import * as sharp from 'sharp';
 import { ensureDir, writeFile } from "fs-extra";
 import { FileElementResponse } from "./dto/file-element.response";
+import { MFile } from "./mFile.class";
 
 @Injectable()
 export class FilesService {
-  async saveFiles(files: Express.Multer.File[]): Promise<FileElementResponse[]> {
+  async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
     const dateFolder = format(new Date(), 'yyyy-MM-dd');
     const uploadFolder = `${path}/uploads/${dateFolder}`;
 
@@ -21,5 +23,9 @@ export class FilesService {
     }
 
     return response;
+  }
+
+  async convertToWebP(file: Buffer): Promise<Buffer> {
+    return sharp(file).webp().toBuffer()
   }
 }
